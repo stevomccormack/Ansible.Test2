21c4960f219349e689e26cea2eeb34bd026176a1
Steve McCormack <stevomccormack@live.com>
2018-05-14 13:27:28 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ansible
+{
+    class Program
+    {
+        private static readonly IEnumerable<int> _sequence = Enumerable.Range(1, 100);
+
+        static void Main(string[] args)
+        {
+            foreach (var n in _sequence)
+            {
+                if( n % 3 == 0 && n % 5 == 0)
+                    Console.Write($"{n} Ansible Australia\n" );
+                if (n % 3 == 0)
+                    Console.Write($"{n} Ansible\n");
+                if (n % 5 == 0)
+                    Console.Write($"{n} Australia\n");
+
+            }
+            Console.ReadLine();
+        }
+
+        //TODO: Unit Tests
+    }
+}
